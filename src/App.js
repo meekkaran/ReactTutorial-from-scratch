16352de8ffbf0fe10ed0d904b10fdeb146f57ff4
meekkaran <meekkaran@gmail.com>
2020-11-24 20:55:43 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import  Greet from './components/Greet'
+import Welcome from './components/Welcome'
+import Hello from './components/Hello'
+import Message from './components/Message'
+
+function App() {
+  return (
+    <div className="App">
+      <Message />
+     {/*<Greet/>*/}
+      {/*<Welcome/>*/}
+      {/*<Hello/>*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Greet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+//function Greet(){
+//    return <h1>Hello Meek</h1>
+//}
+
+const Greet = () => <h1>Hello meek plesase be good</h1>
+
+export default Greet
