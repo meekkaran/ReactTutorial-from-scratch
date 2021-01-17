12734daaacc89255f34f903da03618d3397f5afa
Meek Karan <meekkaran@gmail.com>
2021-01-17 08:10:37 +0300

    morning commit

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -16,12 +16,18 @@ import Inline from './components/Inline';
 import './appStyles.css'
 import styles from './appStyles.module.css'
 import Form from './components/Form';
+import LifecycleA from './components/LifecycleA';
+import FragmentDemo from './components/FragmentDemo';
+import Table from './components/Table';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Form />
+        <Table />
+        {/* <FragmentDemo /> */}
+        {/* <LifecycleA /> */}
+        {/* <Form /> */}
         {/* <h1 className='error'>Error</h1>
         <h1 className={styles.success}>Success</h1>
         <Inline /> */}
